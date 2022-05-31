d0c3beacb093d746917bc49dde0f005082a00af8
Dani-Hrdz <105619891+Dani-Hrdz@users.noreply.github.com>
2022-05-31 11:25:51 -0700

    programa 1
    es el primer programa realizado en la clase

---
+++ Escritura-Archivos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Escritura_Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt",true);
+
+            string[] lines = {
+                "Esta es la nueva escritura",
+                "Esta es la segunda linea",
+                "Fin del texto"
+            };
+            foreach(string line in lines)
+            {
+                sw.WriteLine(line);//escribe en el archivo
+            }
+            sw.Close();//se cierra el archivo
+            Console.WriteLine("Escribiendo en el archivo...");
+            Console.ReadLine();
+        }
+    }
+}
